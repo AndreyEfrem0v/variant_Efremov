a02990e502c00a53d51aa67d683fb729d771f961
AndreyEfrem0v <134065354+AndreyEfrem0v@users.noreply.github.com>
2023-05-19 15:53:41 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+import random
+
+# Создаем списки A и B по 12 элементов
+A = [random.randint(1, 100) for i in range(12)]
+B = [random.randint(1, 100) for i in range(12)]
+
+# Создаем список C по правилу c[i]=max(a[i],b[i])
+C = [max(A[i], B[i]) for i in range(12)]
+
+# Выводим списки A, B и C
+print("Список A: ", A)
+print("Список B: ", B)
+print("Список C: ", C)
 
